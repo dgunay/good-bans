ff27d9088ddf8561b8c6995f858712085ee6c553
Devin Gunay <devingunay@gmail.com>
2018-05-20 22:48:37 -0700

    remove old copypaste test cases

--- test/ChampionsDatabaseTest.php
+++ test/ChampionsDatabaseTest.php
@@ -17,68 +17,11 @@ final class ChampionsDatabaseTest // extends TestCase
 			new ChampionGG(),
 			new RiotChampions()
 		);
-		$this->db->load_csv(__DIR__ . '/datasets/simple.csv');
 	}
+
 	protected function tearDown() {
 		$this->db = null;
 	}
-	public function test_visit() {
-    $this->db->visit('Chipotle');
-    $data = $this->db->get_all_rows();
-		$expected = array(
-			array(
-				"Chipotle",
-				"6",
-				"mexican",
-			),
-			array(
-				"Santa Monica Seafood",
-				"10",
-				"seafood",
-			),
-    );
-    
-    $this->assertEquals($expected, $data);
-  }
-  
-	public function test_visit_new_restaurant() {
-    $this->db->visit('McDonalds', 'fast food');
-    $data = $this->db->get_all_rows();
-		$expected = array(
-			array(
-				"Chipotle",
-				"5",
-				"mexican",
-			),
-			array(
-				"Santa Monica Seafood",
-				"10",
-				"seafood",
-			),
-			array(
-				"McDonalds",
-				"1",
-				"fast food",
-			),
-    );
-    $this->assertEquals($expected, $data);
-  }
-	public function test_least_visited_restaurant() {
-		// TODO: create a test case for ties
-    $least_visited = $this->db->least_visited_restaurant();
-    $expected = 'Chipotle';
-    
-    $this->assertEquals($expected, $least_visited);
-	}
-	public function test_remove() {
-    $this->db->remove('Chipotle');
-    $expected = array(
-      array(
-				"Santa Monica Seafood",
-				"10",
-				"seafood",
-			),
-    );
-    $this->assertEquals($expected, $this->db->get_all_rows());
-	}
+
+	
 }
