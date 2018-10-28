1b69afaf6dec9d323c001cb852c670eaa2a2e284
dgunay <devingunay@gmail.com>
2018-10-27 20:54:23 -0700

    add db test for Lolalytics

--- test/ChampionsDatabaseTest.php
+++ test/ChampionsDatabaseTest.php
@@ -4,31 +4,32 @@ namespace GoodBans\Test;
 
 use GoodBans\ChampionsDatabase;
 use GoodBans\Test\Mock\OpGG;
+use GoodBans\Test\Mock\Lolalytics;
 use GoodBans\Test\Mock\RiotChampions;
 use GoodBans\Logger;
 use PHPUnit\Framework\TestCase;
 
 final class ChampionsDatabaseTest extends TestCase
 {
-	protected $db;
-
-	protected function setUp() {
-		$this->db = new ChampionsDatabase(
+	public function testOpGG() {
+		$db = new ChampionsDatabase(
 			new \PDO('sqlite::memory:'),
 			new OpGG(),
 			new RiotChampions(),
 			new Logger(fopen('php://memory', 'w'))
 		);
 
-		$this->db->refresh();
-	}
-
-	public function testThing() {
 		$this->assertTrue(true);
 	}
-	
-	protected function tearDown() {
-		$this->db = null;
-	}
 
+	public function testLolalytics() {
+		$db = new ChampionsDatabase(
+			new \PDO('sqlite::memory:'),
+			new Lolalytics(),
+			new RiotChampions(),
+			new Logger(fopen('php://memory', 'w'))
+		);
+
+		$this->assertTrue(true);
+	}
 }
